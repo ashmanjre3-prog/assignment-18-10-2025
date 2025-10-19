6a3d382b3f2db06caf8e5fa35b5cda969a66c613
ashmanjre3-prog <ashmanjre3@gmail.com>
2025-10-19 09:34:49 +0530

    created listing page, filter and dropdown functionality

---
+++ src/Components/redux/userSlices/listAllUsers.js
@@ -0,0 +1,45 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+// Async thunk to fetch users
+export const fetchUsers = createAsyncThunk(
+  "users/fetchUsers",
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+
+      return response?.data; // This will be your payload
+    } catch (error) {
+      return rejectWithValue(error.response?.data || "Error fetching users");
+    }
+  }
+);
+
+const userSlice = createSlice({
+  name: "users",
+  initialState: {
+    list: [],
+    loading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchUsers.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchUsers.fulfilled, (state, action) => {
+        state.loading = false;
+        state.list = action.payload;
+      })
+      .addCase(fetchUsers.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  },
+});
+
+export default userSlice.reducer;
--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./Components/redux/store.js";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <StrictMode>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </StrictMode>
   </BrowserRouter>
 );
